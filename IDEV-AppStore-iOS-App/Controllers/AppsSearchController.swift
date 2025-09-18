9b5955bad59ece060bac7fbab3ae262295041b76
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-18 18:53:59 +0300

    Added a fetchiTunesApps() method with basic functioanality

--- IDEV-AppStore-iOS-App/Controllers/AppsSearchController.swift
+++ IDEV-AppStore-iOS-App/Controllers/AppsSearchController.swift
@@ -1,5 +1,17 @@
 import UIKit
 
+// https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+//
+// https://performance-partners.apple.com/search-api
+//
+// https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/SearchExamples.html#//apple_ref/doc/uid/TP40017632-CH6-SW1
+//
+// http://itunes.apple.com/search?term=jack+johnson
+// http://itunes.apple.com/search?term=instagram&entity=software
+//
+// https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/Searching.html
+// https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/LookupExamples.html
+
 final class AppsSearchController: UICollectionViewController {
 
     fileprivate let cellIdentifier = "SearchResultCell"
@@ -10,6 +22,26 @@ final class AppsSearchController: UICollectionViewController {
         collectionView.backgroundColor = .white
         
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        
+        fetchiTunesApps()
+    }
+    
+    fileprivate func fetchiTunesApps() {
+        let urlString = "http://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error {
+                print("Failed to fetch apps: ", error)
+                return
+            }
+            
+            if let data {
+                let s = String(data: data, encoding: .utf8)
+                print(s)
+            }
+            
+        }.resume()
     }
     
     init() {
@@ -28,7 +60,11 @@ final class AppsSearchController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView,
                                  cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: cellIdentifier, for: indexPath
+        ) as! SearchResultCell
+        
+        cell.nameLabel.text = "HERE IS MY APP NAME"
         
         return cell
     }
