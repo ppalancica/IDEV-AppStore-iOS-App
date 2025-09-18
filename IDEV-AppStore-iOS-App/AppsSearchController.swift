8c7cf9f1f050a8f229955bd34bab5c00fc142ef2
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-18 12:42:12 +0300

    Added some basic functionality in AppsSearchController

--- IDEV-AppStore-iOS-App/AppsSearchController.swift
+++ IDEV-AppStore-iOS-App/AppsSearchController.swift
@@ -2,10 +2,14 @@ import UIKit
 
 final class AppsSearchController: UICollectionViewController {
 
+    fileprivate let cellIdentifier = "AppsSearchCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .lightGray
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
     }
     
     init() {
@@ -14,5 +18,30 @@ final class AppsSearchController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+extension AppsSearchController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 250)
     }
 }
