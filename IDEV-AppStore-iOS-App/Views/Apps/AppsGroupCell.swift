5cd2a2fda386cbc36585187582151603a0a97a01
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-18 23:37:57 +0300

    Embedded a UIViewController inside AppsGroupCell

--- IDEV-AppStore-iOS-App/Views/Apps/AppsGroupCell.swift
+++ IDEV-AppStore-iOS-App/Views/Apps/AppsGroupCell.swift
@@ -1,13 +1,20 @@
 import UIKit
 
+extension UILabel {
+    
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        
+        self.text = text
+        self.font = font
+    }
+}
+
 final class AppsGroupCell: UICollectionViewCell {
     
-    let tittleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "App Section"
-        label.font = .boldSystemFont(ofSize: 30)
-        return label
-    }()
+    let tittleLabel = UILabel(text: "App Section", font: .boldSystemFont(ofSize: 30))
+    
+    let horizontalController = UIViewController()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -17,6 +24,10 @@ final class AppsGroupCell: UICollectionViewCell {
         contentView.addSubview(tittleLabel)
         // tittleLabel.fillSuperview()
         tittleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor)
+        
+        contentView.addSubview(horizontalController.view)
+        horizontalController.view.backgroundColor = .blue
+        horizontalController.view.anchor(top: tittleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
     }
     
     required init?(coder: NSCoder) {
