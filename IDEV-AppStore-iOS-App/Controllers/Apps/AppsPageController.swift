fec0613b05b2e9b6b1a35a55959b478831e8c591
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-19 12:11:33 +0300

    Added a Header View to AppsPageController

--- IDEV-AppStore-iOS-App/Controllers/Apps/AppsPageController.swift
+++ IDEV-AppStore-iOS-App/Controllers/Apps/AppsPageController.swift
@@ -3,12 +3,36 @@ import UIKit
 final class AppsPageController: BaseListController {
     
     fileprivate let cellIdentifier = "AppsGroupCell"
+    fileprivate let headerIdentifier = "AppsGroupHeader"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
-        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        
+        collectionView.register(AppsGroupCell.self,
+                                forCellWithReuseIdentifier: cellIdentifier)
+        
+        collectionView.register(AppsPageHeader.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: headerIdentifier)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                                 viewForSupplementaryElementOfKind kind: String,
+                                 at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                     withReuseIdentifier: headerIdentifier,
+                                                                     for: indexPath)
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 300)
     }
     
     override func collectionView(_ collectionView: UICollectionView,
---
+++ IDEV-AppStore-iOS-App/Views/Apps/AppsPageHeader.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class AppsPageHeader: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
