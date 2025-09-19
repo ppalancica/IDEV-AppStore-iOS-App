b519b4095e37a69f912c51321c7cdf00b972312f
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-19 23:54:18 +0300

    More UI adjustments to AppDetailCell

--- IDEV-AppStore-iOS-App/Views/Apps/Details/AppDetailCell.swift
+++ IDEV-AppStore-iOS-App/Views/Apps/Details/AppDetailCell.swift
@@ -2,13 +2,52 @@ import UIKit
 
 final class AppDetailCell: UICollectionViewCell {
     
+    let appIconImageView = UIImageView(cornerRadius: 16)
+    let nameLabel = UILabel(text: "App Name", font: .boldSystemFont(ofSize: 24), numberOfLines: 2)
+    let priceButton = UIButton(title: "$4.99")
+    let whatsNewLabel = UILabel(text: "What's New", font: .boldSystemFont(ofSize: 20))
+    let releaseNotesLabel = UILabel(text: "Release Notes", font: .systemFont(ofSize: 16), numberOfLines: 0)
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        appIconImageView.backgroundColor = .red
+        appIconImageView.constrainWidth(constant: 140)
+        appIconImageView.constrainHeight(constant: 140)
+        
+        priceButton.backgroundColor = UIColor(red: 50 / 255.0, green: 120 / 255.0, blue: 240 / 255.0, alpha: 1.0)
+        priceButton.constrainHeight(constant: 32)
+        priceButton.layer.cornerRadius = 32 / 2
+        priceButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        priceButton.setTitleColor(.white, for: .normal)
+        priceButton.constrainWidth(constant: 80)
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            UIStackView(arrangedSubviews: [
+                appIconImageView,
+                VerticalStackView(arrangedSubviews: [
+                    nameLabel,
+                    UIStackView(arrangedSubviews: [priceButton, UIView()]),
+                    UIView()
+                ], spacing: 12)
+            ], customSpacing: 20),
+            whatsNewLabel,
+            releaseNotesLabel
+        ], spacing: 16)
+        
+        addSubview(stackView)
+        stackView.fillSuperview(padding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIStackView {
+    
+    convenience init(arrangedSubviews: [UIView], customSpacing: CGFloat = 0) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.spacing = customSpacing
     }
 }
